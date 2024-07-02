b25792bbd0aa462f60f1f4baef1903e0cc6ae601
DevonWoodruff <devon@sunsab.com>
2024-07-02 13:13:50 -0600

    testing tournament page

--- app/brackettest/bracket/Bracket.jsx
+++ app/brackettest/bracket/Bracket.jsx
@@ -1,9 +1,19 @@
-'use client';
+"use client";
 
-import { SingleEliminationBracket, DoubleEliminationBracket, Match, SVGViewer,createTheme } from '@g-loot/react-tournament-brackets';
-import {  dataDoublePlayoffs, walkOverData } from './mock_bracket_data';
+import {
+  SingleEliminationBracket,
+  DoubleEliminationBracket,
+  Match,
+  SVGViewer,
+  createTheme,
+} from "@g-loot/react-tournament-brackets";
+import { dataDoublePlayoffs, walkOverData } from "./mock_bracket_data";
+import { getTournaments, updateTournament, getUsers, getUserRef  } from "@/app/_utils/firebase_services";
+import { useUserAuth } from "@/app/_utils/auth-context.js";
+import { useState, useEffect } from "react";  
 
-export const DoubleElimination = ({matches}) => (
+
+export const DoubleElimination = ({ matches }) => (
   <DoubleEliminationBracket
     matches={matches}
     matchComponent={Match}
@@ -26,7 +36,7 @@ export const DoubleElimination = ({matches}) => (
   />
 );
 
-export const SingleElimination = ({matches}) => (
+export const SingleElimination = ({ matches }) => (
   <SingleEliminationBracket
     matches={matches}
     matchComponent={Match}
@@ -39,55 +49,48 @@ export const SingleElimination = ({matches}) => (
 );
 
 const WhiteTheme = createTheme({
-  textColor: { main: '#000000', highlighted: '#07090D', dark: '#3E414D' },
-  matchBackground: { wonColor: '#daebf9', lostColor: '#96c6da' },
+  textColor: { main: "#000000", highlighted: "#07090D", dark: "#3E414D" },
+  matchBackground: { wonColor: "#daebf9", lostColor: "#96c6da" },
   score: {
-    background: { wonColor: '#87b2c4', lostColor: '#87b2c4' },
-    text: { highlightedWonColor: '#7BF59D', highlightedLostColor: '#FB7E94' },
+    background: { wonColor: "#87b2c4", lostColor: "#87b2c4" },
+    text: { highlightedWonColor: "#7BF59D", highlightedLostColor: "#FB7E94" },
   },
   border: {
-    color: '#CED1F2',
-    highlightedColor: '#da96c6',
+    color: "#CED1F2",
+    highlightedColor: "#da96c6",
   },
-  roundHeader: { backgroundColor: '#da96c6', fontColor: '#000' },
-  connectorColor: '#CED1F2',
-  connectorColorHighlight: '#da96c6',
-  svgBackground: '#FAFAFA',
+  roundHeader: { backgroundColor: "#da96c6", fontColor: "#000" },
+  connectorColor: "#CED1F2",
+  connectorColorHighlight: "#da96c6",
+  svgBackground: "#FAFAFA",
 });
 
 export const Bracket = () => {
-    const singleEliminationMatches = [
-        {
-          "id": 260005,
-          "name": "Final - Match",
-          "nextMatchId": null, // Id for the nextMatch in the bracket, if it's final match it must be null OR undefined
-          "tournamentRoundText": "4", // Text for Round Header
-          "startTime": "2021-05-30",
-          "state": "DONE", // 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | 'DONE' | 'SCORE_DONE' Only needed to decide walkovers and if teamNames are TBD (to be decided)
-          "participants": [
-            {
-              "id": "c016cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc", // Unique identifier of any kind
-              "resultText": "WON", // Any string works
-              "isWinner": false,
-              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY' | null
-              "name": "giacomo123"
-            },
-            {
-              "id": "9ea9ce1a-4794-4553-856c-9a3620c0531b",
-              "resultText": null,
-              "isWinner": true,
-              "status": null, // 'PLAYED' | 'NO_SHOW' | 'WALK_OVER' | 'NO_PARTY'
-              "name": "Ant"
-            }
-          ]
-        }
-    ]
+  const [tournaments, setTournaments] = useState([]);
+  const [bracket, setBracket] = useState(null);
+  const [users, setUsers] = useState(null);
+  const { user } = useUserAuth();
 
-    return (
-        <div>
-          
-          <DoubleEliminationBracket matches={dataDoublePlayoffs} />
-          <SingleElimination matches={walkOverData} />   
-        </div>
-    )
-}
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const data = await getUsers();
+      console.log(data);
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
+  const handleNewBracket = async () => {
+    const newBracket = await createBracket();
+    const newNewBracket = await initializeMatches(newBracket, users);
+    console.log("NewNewBracket:");
+    console.log(newNewBracket);
+    setBracket(newNewBracket);
+  };
+
+  return (
+    <div>
+      <SingleElimination matches={walkOverData} />
+    </div>
+  );
+};
