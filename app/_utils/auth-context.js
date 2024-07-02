3eb288bfa5d31c0b372a4abf60dc4e5f4e1e9fd8
Charles <charles@sunsab.com>
2024-07-02 12:36:23 -0600

    Updates auth-context user to contain user DB data
    Firebase auth data is now in user.auth

--- app/_utils/auth-context.js
+++ app/_utils/auth-context.js
@@ -35,21 +35,44 @@ export const AuthContextProvider = ({ children }) => {
 		return signOut(auth);
 	};
 	
+	//Create and pass user data to context
+	//Database information is stored in user
+	//Auth information is stored in user.auth, in case it needs to be passed back to the authenticator
+	//dbUser will be deprecated
 	useEffect(() => {
+		//Async function to fetch user data from Firestore
+		const fetchUser = async (uid) => {
+			const newUser = await getUser(uid);
+			return newUser;
+		};
 		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-		setUser(currentUser);
+			//If user is signed in, fetch user data and set it
+			if (currentUser)
+				fetchUser(currentUser.uid).then((newUser) => {
+					setDBUser(newUser);
+					setUser({ auth: currentUser, ...newUser });
+					//console.log("AUTH-CONTEXT: Auth state changed:")
+					//console.log({ auth: currentUser, ...newUser });
+				});
+			//If user is signed out, set user to null
+			else
+				setUser(null);
+			
 		});
+		//It's called unsubscribe because it's a function that will unsubscribe the onAuthStateChanged listener
+		//Aaron wrote it that way so I'm keeping it
 		return () => unsubscribe();
-	}, [user]);
+		
+	}, [auth.currentUser]);
 	
-	useEffect(() => {
+	/*useEffect(() => {
 		const fetchUser = async () => {
 			const newUser = await getUser(user.uid);
 			setDBUser(newUser);
 		};
 		if (user)
 			fetchUser();
-	}, [user]);
+	}, [user]);*/
 	
 	return (
 		<AuthContext.Provider value={{dbUser, user, gitHubSignIn, firebaseSignOut, emailPasswordSignIn, emailPasswordSignUp }}>
